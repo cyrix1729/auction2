7ed4f75e5a5c97ef4fdede6f261f82fc1a4c92b0
Faris <77870664+cyrix1729@users.noreply.github.com>
2022-12-08 06:47:08 +0000

    Delete auction directory

--- auction/auctionApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('getUsersQuestions/<int:user_id>/', views.getUserQuestions, name='questions'),
-]
